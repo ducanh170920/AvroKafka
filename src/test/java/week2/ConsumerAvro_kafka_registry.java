0503445ebef21b87712790a4471d78c022d8443e
ducanh170920 <ducanhchelseasfc@gmail.com>
2021-06-09 14:01:14 +0700

    Push excercise week 2

---
+++ src/test/java/week2/ConsumerAvro_kafka_registry.java
@@ -0,0 +1,40 @@
+package week2;
+
+import io.confluent.kafka.serializers.KafkaAvroDeserializer;
+import io.confluent.kafka.serializers.KafkaAvroDeserializerConfig;
+import io.confluent.kafka.serializers.KafkaAvroSerializer;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.sql.SQLOutput;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Properties;
+
+public class ConsumerAvro_kafka_registry {
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,"10.140.0.13:9092");
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "io.confluent.kafka.serializers.KafkaAvroDeserializer");
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "group2");
+        properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        properties.put(KafkaAvroDeserializerConfig.SCHEMA_REGISTRY_URL_CONFIG, "http://10.140.0.13:8081");
+
+        KafkaConsumer<String, GenericRecord>consumer = new KafkaConsumer<String, GenericRecord>(properties);
+
+        consumer.subscribe(Arrays.asList("anhhd25_serilizer3"));
+
+        while (true){
+            ConsumerRecords<String,GenericRecord> consumerRecords = consumer.poll(100);
+            for(ConsumerRecord<String,GenericRecord> record : consumerRecords){
+                System.out.println(record.value());
+            }
+            consumer.commitAsync();
+
+        }
+
+    }
+}
