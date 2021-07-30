727cf4e8f93f83108aebd1eecdae9a549cfaa036
ducanh170920 <56523371+ducanh170920@users.noreply.github.com>
2021-07-30 15:47:07 +0700

    Add files via upload

---
+++ src/java/week8/main.py
@@ -0,0 +1,41 @@
+import json
+import re
+from pyvi import ViTokenizer, ViPosTagger
+def nomalize(data):
+
+    for j in range(len(data)):
+        data[j] = data[j].replace("_", " ").lower()
+    result = []
+    #filter
+    for word in data:
+        if(re.search("^\w+\s", word)):
+            result.append(word)
+    data = list(dict.fromkeys(data))
+    return result;
+
+lines = open("C:\\Users\\hoang\\OneDrive\\Desktop\\data\\data_1.json","r",encoding="utf8")
+f    = open("C:\\Users\\hoang\\OneDrive\\Desktop\\data\\data_anhhd25_1.json","a",encoding="utf8")
+i = 0
+x = 0
+for line in lines:
+    x = x + 1
+    i = i + 1
+    if i == 1:
+       f.write(line)
+    if i == 2:
+        i = 0
+        title = json.loads(line)['title']
+        data = ViTokenizer.tokenize(title).split(" ")
+        data = nomalize(data)
+        print(data)
+        patterm = "{" + "\"suggest_title\": [\"" + '", "'.join(data) + "\"]}\n"
+        f.write(patterm)
+
+
+f.close()
+lines.close()
+
+
+
+
+
